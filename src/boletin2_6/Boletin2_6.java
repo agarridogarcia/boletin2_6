66a48f29bfcd5d6fad6bb4e0aceecc8ab1b7d175
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-04 08:40:30 +0200

    Descuento

---
+++ src/boletin2_6/Boletin2_6.java
@@ -0,0 +1,21 @@
+
+package boletin2_6;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin2_6 {
+
+ 
+    public static void main(String[] args) {
+        float precio_pagado, precio_tarifa, descuento;
+        precio_pagado=Float.parseFloat(JOptionPane.showInputDialog("Precio pagado"));
+        precio_tarifa=Float.parseFloat(JOptionPane.showInputDialog("Precio tarifa"));
+        descuento= 100 * precio_tarifa - precio_pagado / precio_tarifa;
+        
+        JOptionPane.showInputDialog("descuento="+descuento);  
+        
+
+    }
+    
+}
