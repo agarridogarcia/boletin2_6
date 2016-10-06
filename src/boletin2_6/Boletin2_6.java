9d12b7adfa644055eebf4a024012e243da281f19
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-06 09:38:13 +0200

    Descuento

--- src/boletin2_6/Boletin2_6.java
+++ src/boletin2_6/Boletin2_6.java
@@ -8,10 +8,11 @@ public class Boletin2_6 {
 
  
     public static void main(String[] args) {
+        //queremos hayar el porcentaje de descuento
         float precio_pagado, precio_tarifa, descuento;
         precio_pagado=Float.parseFloat(JOptionPane.showInputDialog("Precio pagado"));
         precio_tarifa=Float.parseFloat(JOptionPane.showInputDialog("Precio tarifa"));
-        descuento= 100 * precio_tarifa - precio_pagado / precio_tarifa;
+        descuento= 100* (precio_tarifa - precio_pagado) / precio_tarifa;
         
         JOptionPane.showInputDialog("descuento="+descuento);  
         
